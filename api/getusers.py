f83732dc5af069707488893a3b3537ee3ecc55ca
yuricoder07 <yurisub07@gmail.com>
2023-02-16 11:30:48 -0800

    search

--- api/getusers.py
+++ api/getusers.py
@@ -5,12 +5,16 @@ import json
 getusers_api = Blueprint('getusers_api', __name__, url_prefix='/api/getusers')
 api = Api(getusers_api)
 
+def getUsers():
+	counter = 0
+	with open('users.json') as menu_file:
+		items = json.load(menu_file)
+		return(len(items))
+	if counter == 0:
+		return {"No Users"}
 
 class usersAPI:
 	class _Read(Resource):
 		def get(self):
-			counter = 0
-			with open('users.json') as menu_file:
-				items = json.load(menu_file)
-				return(len(items))
+			return(getUsers())
 	api.add_resource(_Read, '/')
