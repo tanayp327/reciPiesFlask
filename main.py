9c188d68c2820f9985900476328bad2f9a2997c5
Soham360 <sohamkamat10@gmail.com>
2023-05-01 23:46:10 -0700

    e

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import threading
 
 # import "packages" from flask
 from flask import render_template  # import render_template from "public" flask libraries
+
 # import "packages" from "this" project
 from __init__ import app  # Definitions initialization
 from model.jokes import initJokes
@@ -48,5 +49,4 @@ if __name__ == "__main__":
     # change name for testing
     from flask_cors import CORS
     cors = CORS(app)
-    CORS(app)
     app.run(debug=True, host="0.0.0.0", port="8086")
